dd7620969c95e9a5594ecd108440c21d4947b788
Derek Reynolds <myn_2000@yahoo.com>
2010-10-17 23:24:52 -0400

    Configured URL Pointings and customizations pointing to github

--- src/warmupdaterapp/customization/Customization.java
+++ src/warmupdaterapp/customization/Customization.java
@@ -2,19 +2,19 @@ package warmupdaterapp.customization;
 
 public class Customization {
     //The String from the build.prop before the Version
-    public static final String RO_MOD_START_STRING = "CyanogenMod-";
+    public static final String RO_MOD_START_STRING = "myn_Warm_TwoPointTwo_RLS";
     //Minimum Supported Version (So the User has to install google apps and so before)
-    public static final String MIN_SUPPORTED_VERSION_STRING = RO_MOD_START_STRING + "4.1.99";
+    public static final String MIN_SUPPORTED_VERSION_STRING = RO_MOD_START_STRING + "2";
     //Updateinstructions for the min supported Version
-    public static final String UPDATE_INSTRUCTIONS_URL = "http://wiki.cyanogenmod.com/index.php/Upgrading_From_Older_CyanogenMod_or_other_rooted_ROMs";
+    public static final String UPDATE_INSTRUCTIONS_URL = "http://forum.xda-developers.com/showthread.php?t=793471";
     //DB filename
-    public static final String DATABASE_FILE = "cmupdater.db";
+    public static final String DATABASE_FILE = "warmupdater.db";
     //DownloadDirectory
     public static final String DOWNLOAD_DIR = "download";
     //MUST be the first package name.
-    public static final String PACKAGE_FIRST_NAME = "cmupdaterapp";
+    public static final String PACKAGE_FIRST_NAME = "warmupdaterapp";
     //Filename for Instance save
-    public static final String STORED_STATE_FILENAME = "cmupdater.state";
+    public static final String STORED_STATE_FILENAME = "warmupdater.state";
     //Android Board type
     public static final String BOARD = "ro.product.board";
     //Name of the Current Rom
